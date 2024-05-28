01892f2a172f7fcf6587991e72ff61da80bb9675
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-05-28 05:36:09 +0100

    Height adjustmants/ code clean up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { data } from "./data";
 
 function App() {
   return (
-    <div className="App" style={{ height: "500px" }}>
+    <div className="App" style={{ height: "80vh" }}>
       <MyResponsiveBar data={data} />
     </div>
   );
--- src/BarChart.js
+++ src/BarChart.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
-import { data } from "./data";
 
 const MyResponsiveBar = ({ data }) => (
   <ResponsiveBar
